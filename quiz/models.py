b27a911c17f273dc5a868f4fe0a19cc84b4b91be
dhilip454 <68657736+dhilip454@users.noreply.github.com>
2021-01-06 23:00:08 +0530

    Update models.py

--- quiz/models.py
+++ quiz/models.py
@@ -30,7 +30,7 @@ class Category(models.Model):
 
     category = models.CharField(
         verbose_name=_("Category"),
-        max_length=255, blank=True,
+        max_length=250, blank=True,
         unique=True, null=True)
 
     objects = CategoryManager()
@@ -46,13 +46,15 @@ class Category(models.Model):
 class Quiz(models.Model):
 
     title = models.CharField(
-        verbose_name=_("Title"), blank=False)
+        verbose_name=_("Title"),
+        max_length=60, blank=False)
 
     description = models.TextField(
         verbose_name=_("Description"),
         blank=True, help_text=_("a description of the quiz"))
 
-    url = models.SlugField( blank=False,
+    url = models.SlugField(
+        max_length=60, blank=False,
         help_text=_("a user friendly url"),
         verbose_name=_("user friendly url"))
 
